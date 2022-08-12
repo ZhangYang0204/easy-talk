f87fc4e41fb5eb7714c54f11debc490847d3db11
ZhangYang <3319527246@qq.com>
2022-08-12 22:29:15 +0800

    1.4.3->1.5.0： 新增： - 新增了在第一个页面的返回按钮 删除： - 删除了指令/EasyTalk privateChat，删除的原因是这不符合我玩家纯Gui的原则

--- src/main/java/pers/zhangyang/easytalk/domain/MainOptionPage.java
+++ src/main/java/pers/zhangyang/easytalk/domain/MainOptionPage.java
@@ -1,14 +1,18 @@
 package pers.zhangyang.easytalk.domain;
 
+import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
+import pers.zhangyang.easylibrary.base.BackAble;
 import pers.zhangyang.easylibrary.base.GuiPage;
 import pers.zhangyang.easylibrary.base.SingleGuiPageBase;
 import pers.zhangyang.easytalk.yaml.GuiYaml;
 
-public class MainOptionPage extends SingleGuiPageBase {
+import java.util.List;
+
+public class MainOptionPage extends SingleGuiPageBase implements BackAble {
 
     public MainOptionPage(Player viewer, GuiPage backPage, OfflinePlayer owner) {
         super(GuiYaml.INSTANCE.getString("gui.title.mainOptionPage"), viewer, backPage,owner);
@@ -22,10 +26,36 @@ public class MainOptionPage extends SingleGuiPageBase {
         ItemStack shout = GuiYaml.INSTANCE.getButton("gui.button.mainOptionPage.shout");
         inventory.setItem(21, shout);
 
+        ItemStack back = GuiYaml.INSTANCE.getButton("gui.button.mainOptionPage.back");
+        inventory.setItem(49, back);
 
         viewer.openInventory(this.inventory);
 
 
     }
 
+    @Override
+    public void back() {
+        List<String> cmdList= GuiYaml.INSTANCE.getStringList("gui.firstPageBackCommand");
+        if (cmdList==null){
+            return;
+        }
+        for (String s:cmdList){
+            String[] args=s.split(":");
+            if (args.length!=2){
+                continue;
+            }
+            String way=args[0];
+            String command=args[1];
+            if ("console".equalsIgnoreCase(way)){
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(),command);
+            }else if ("player".equalsIgnoreCase(way)){
+                Bukkit.dispatchCommand(viewer,command);
+            }else if ("operator".equalsIgnoreCase(way)){
+                viewer.setOp(true);
+                Bukkit.dispatchCommand(viewer,command);
+                viewer.setOp(false);
+            }
+        }
+    }
 }
